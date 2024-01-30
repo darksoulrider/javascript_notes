741f82f1e58d3f04425382054ef262959a591249
Mukesh Rathod <81797236+darksoulrider@users.noreply.github.com>
2024-01-30 09:03:39 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,23 +1,5 @@
-var timetest = (fn, t) => {
-    return (...args) => {
-        const rejtime = new Promise((res, rej) => {
-            setTimeout(() => {
-                rej("Time limit exceeded.")
-            }, t)
-        })
-        return Promise.race([fn(...args), rejtime])
-    }
-}
+<script>
+    alert(1)
+    </script> 
 
-async function fn(n) {
-    await new Promise(res => setTimeout(res, 100));
-    return n * n;
-}
-
-const data = timetest(fn, 200);
-
-data(30).then((val) => {
-    console.log(val)
-}).catch((err) => {
-    console.log(err)
-})
+alert("I am")
